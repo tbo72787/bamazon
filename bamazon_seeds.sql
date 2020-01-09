59e79530927a1471cb770b2de69dd556bffc0387
Taylor Zettel <tzettel@gmail.com>
2020-01-09 16:51:01 -0500

    added primary key to schema.sql, added 'use bamazon' command and semicolons to bamazon_seeds.sql

--- bamazon_seeds.sql
+++ bamazon_seeds.sql
@@ -1,29 +1,31 @@
-INSERT INTO products (product_name, department, price, stock_quantity)
-VALUES ("tv", "electronics", 749.99, 15)
+USE bamazon;
 
 INSERT INTO products (product_name, department, price, stock_quantity)
-VALUES ("cd", "electronics", 19.99, 75)
+VALUES ("tv", "electronics", 749.99, 15);
 
 INSERT INTO products (product_name, department, price, stock_quantity)
-VALUES ("ipad", "electronics", 999.99, 25)
+VALUES ("cd", "electronics", 19.99, 75);
 
 INSERT INTO products (product_name, department, price, stock_quantity)
-VALUES ("tylenol", "pharmacy", 14.99, 105)
+VALUES ("ipad", "electronics", 999.99, 25);
 
 INSERT INTO products (product_name, department, price, stock_quantity)
-VALUES ("advil", "pharmacy", 14.99, 120)
+VALUES ("tylenol", "pharmacy", 14.99, 105);
 
 INSERT INTO products (product_name, department, price, stock_quantity)
-VALUES ("benadryl", "pharmacy", 12.99, 45)
+VALUES ("advil", "pharmacy", 14.99, 120);
 
 INSERT INTO products (product_name, department, price, stock_quantity)
-VALUES ("banana", "grocery", 0.69, 300)
+VALUES ("benadryl", "pharmacy", 12.99, 45);
 
 INSERT INTO products (product_name, department, price, stock_quantity)
-VALUES ("chicken", "grocery", 3.99, 55)
+VALUES ("banana", "grocery", 0.69, 300);
 
 INSERT INTO products (product_name, department, price, stock_quantity)
-VALUES ("pasta", "grocery", 7.99, 95)
+VALUES ("chicken", "grocery", 3.99, 55);
 
 INSERT INTO products (product_name, department, price, stock_quantity)
-VALUES ("prius", "automative", 24325, 5)
+VALUES ("pasta", "grocery", 7.99, 95);
+
+INSERT INTO products (product_name, department, price, stock_quantity)
+VALUES ("prius", "automative", 24325, 5);
--- schema.sql
+++ schema.sql
@@ -9,5 +9,6 @@ CREATE TABLE products (
   product_name VARCHAR(50) NOT NULL,
   department VARCHAR(20) NOT NULL,
   price DECIMAL(10, 2) NOT NULL,
-  stock_quantity INTEGER(20) NOT NULL
+  stock_quantity INTEGER(20) NOT NULL,
+  PRIMARY KEY (item_id)
 );
