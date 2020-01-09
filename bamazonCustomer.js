17b9ed5bbc6d8a33051668ee02e4ef0dd3276863
Taylor Zettel <tzettel@gmail.com>
2020-01-09 17:04:57 -0500

    created bamazonCustomer.js, which connects to bamazon database and displays 'products' table

---
+++ bamazonCustomer.js
@@ -0,0 +1,31 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "F1r3Cr4ck3r",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  readProducts();
+});
+
+function readProducts() {
+  console.log("Selecting all products...\n");
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+    connection.end();
+  });
+}
