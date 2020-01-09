66119141fb96ca1fd6bac974dd7e2efd674efe7d
Taylor Zettel <tzettel@gmail.com>
2020-01-09 17:34:11 -0500

    changed readProducts function to make results more readable

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,10 +22,22 @@ connection.connect(function(err) {
 
 function readProducts() {
   console.log("Selecting all products...\n");
-  connection.query("SELECT * FROM products", function(err, res) {
+  connection.query("SELECT * FROM products", function(err, results) {
     if (err) throw err;
-    // Log all results of the SELECT statement
-    console.log(res);
+    for (var i = 0; i < results.length; i++) {
+      var item_id = results[i].item_id;
+      var product_name = results[i].product_name;
+      var department = results[i].department;
+      var price = results[i].price;
+      var stock_quantity = results[i].stock_quantity;
+      console.log("--------------------");
+      console.log("ID: " + item_id);
+      console.log("Product: " + product_name);
+      console.log("Department: " + department);
+      console.log("Price: " + price);
+      console.log("Quantity: " + stock_quantity);
+    }
+    console.log("--------------------");
     connection.end();
   });
 }
